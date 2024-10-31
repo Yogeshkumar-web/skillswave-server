1e9f6a5eb8c6a25985230db96771cf27b662c68a
Yogesh Kumar <yksainimbd@gmail.com>
2024-10-31 19:41:08 +0530

    add basic app and index code

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,34 @@
-console.log("app");
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+import config from "./config";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// middlewares
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
+app.use(morgan(`${config.logs.morgan}`));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+//routes import
+
+//routes declaration
+app.use("/api/v1/users", () => {});
+
+export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("index");
+import { app } from "./app";
+import config from "./config";
+import connectDB from "./database";
+
+connectDB()
+  .then(() => {
+    app.listen(config.app.port || 8080, () => {
+      console.log(`⚙️ Server is running at port : ${config.app.port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
